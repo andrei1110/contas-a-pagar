2edeaf5d2332fab32ca3d505f267419f378a7adb
Andrei Toledo <actoledo.andrei@gmail.com>
2021-02-28 22:33:56 -0300

    Melhorias de código

--- server/src/__tests__/regras.test.ts
+++ server/src/__tests__/regras.test.ts
@@ -1,11 +1,11 @@
-import { calculaComposto, calculaSimples } from '../controllers';
+import { calcula_composto, calcula_simples } from '../controllers';
 
 test('calcula juros composto', () => {
-    const jurosComposto = calculaComposto(30, 0.3, 5, 12);
+    const jurosComposto = calcula_composto(30, 0.3, 5, 12);
     expect(32.6).toEqual(parseFloat(jurosComposto));
 });
 
 test('calcula juros simples', () => {
-    const jurosSimples = calculaSimples(30, .3, 5, 12);
+    const jurosSimples = calcula_simples(30, .3, 5, 12);
     expect(32.58).toEqual(parseFloat(jurosSimples));
 });
--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -2,17 +2,44 @@ import { Request, Response } from 'express';
 import knex from '../database/connect';
 import moment from 'moment';
 
+interface IRegras {
+    id_regra: number,
+    qtd_dias_inicial: number;
+    qtd_dias_final: number; 
+    qtd_multa: number;
+    qtd_juros: number;
+}
+
+interface IContas {
+    id_conta: number;
+    nome_conta: string;
+    data_vencimento: string;
+    data_pagamento: string;
+    valor_original: number;
+    diferenca_dias: number;
+    id_regra: number;
+    tipo_juros: 'simples' | 'composto';
+    nome_regra: string;
+    qtd_dias_inicial: number;
+    qtd_dias_final: number;
+    qtd_multa: number;
+    qtd_juros: number;
+}
+
 export async function showContas (req: Request, res: Response) {
     try {
-        const contas = await knex('contas')
-            .select('*')
+        const contas:IContas[] = await knex('contas')
+            .select('contas.id_conta', 'contas.nome_conta', 'contas.data_vencimento', 'contas.data_pagamento', 
+                    'contas.valor_original', 'contas.valor_final', 'contas.diferenca_dias', 'contas.id_regra', 
+                    'contas.tipo_juros', 'regras_atraso.nome_regra', 'regras_atraso.qtd_dias_inicial',
+                    'regras_atraso.qtd_dias_final', 'regras_atraso.qtd_multa', 'regras_atraso.qtd_juros')
             .leftJoin('regras_atraso', 'contas.id_regra', 'regras_atraso.id_regra');
 
-        if (contas.length < 1) return res.status(204).json({msg: 'Nenhuma conta encontrada.', contas});
+        if (contas?.length < 1) return res.status(204).json({msg: 'Nenhuma conta encontrada.', contas});
 
         return res.status(200).json({ msg: 'Contas encontradas com sucesso.', contas });
     } catch(e) {
-        if (e && e.code && e.code === 'ECONNREFUSED'){
+        if (e?.code === 'ECONNREFUSED'){
             return res.status(503).json({msg:'Falha na comunicação com o banco de dados. Tente novamente.'});
         }
 
@@ -23,21 +50,23 @@ export async function showContas (req: Request, res: Response) {
 export async function createConta (req: Request, res: Response) {
     try {
         const { nome_conta, valor_original, data_vencimento, data_pagamento, tipo_juros } = req.body;
-        const regras = await knex('regras_atraso').select('*');
+        // const regras:IRegras[] = await knex('regras_atraso')
+        //     .select('id_regra', 'qtd_dias_inicial', 'qtd_dias_final', 'qtd_multa', 'qtd_juros');
         const diferenca_dias = moment(data_pagamento, 'YYYY-MM-DD').diff(moment(data_vencimento, 'YYYY-MM-DD'), 'days');
         let valor_final = valor_original;
-        const regra_aplicada = regras.filter( regra => {
-            if(!regra.qtd_dias_final) regra.qtd_dias_final = Infinity;
-            return regra.qtd_dias_inicial < diferenca_dias && regra.qtd_dias_final >= diferenca_dias;
-        });
+        // const regra_aplicada:IRegras[] = regras.filter( regra => {
+        //     if(!regra.qtd_dias_final) regra.qtd_dias_final = Infinity;
+        //     return regra.qtd_dias_inicial < diferenca_dias && regra.qtd_dias_final >= diferenca_dias;
+        // });
+        const regra_aplicada = await verifica_regra(diferenca_dias);
 
         if(regra_aplicada.length > 0){
             switch (tipo_juros){
                 case 'composto':
-                    valor_final = calculaComposto(valor_original, regra_aplicada[0].qtd_juros, regra_aplicada[0].qtd_multa, diferenca_dias);
+                    valor_final = calcula_composto(valor_original, regra_aplicada[0].qtd_juros, regra_aplicada[0].qtd_multa, diferenca_dias);
                 break;
                 default:
-                    valor_final = calculaSimples(valor_original, regra_aplicada[0].qtd_juros, regra_aplicada[0].qtd_multa, diferenca_dias);
+                    valor_final = calcula_simples(valor_original, regra_aplicada[0].qtd_juros, regra_aplicada[0].qtd_multa, diferenca_dias);
                 break;
             }
         }
@@ -62,7 +91,7 @@ export async function createConta (req: Request, res: Response) {
             id_conta: insert[0]
         });
     } catch (e) {
-        if (e && e.code && e.code === 'ECONNREFUSED'){
+        if (e?.code === 'ECONNREFUSED'){
             return res.status(503).json({msg:'Falha na comunicação com o banco de dados. Tente novamente.'});
         }
 
@@ -74,14 +103,23 @@ export async function createConta (req: Request, res: Response) {
     }
 }
 
-export function calculaComposto(valor_original: number, qtd_juros: number, qtd_multa: number, diferenca_dias: number) {
+export function calcula_composto(valor_original: number, qtd_juros: number, qtd_multa: number, diferenca_dias: number):string {
     let valor_final = valor_original * ( Math.pow( 1 + ( qtd_juros / 100 ) , diferenca_dias ) );
     valor_final += ( valor_original * qtd_multa ) / 100;
     return valor_final.toFixed(2);
 }
 
-export function calculaSimples(valor_original: number, qtd_juros: number, qtd_multa: number, diferenca_dias: number) {
+export function calcula_simples(valor_original: number, qtd_juros: number, qtd_multa: number, diferenca_dias: number):string {
     let valor_final = valor_original + ( ( ( valor_original * qtd_juros) / 100 ) * diferenca_dias);
     valor_final += ( valor_original * qtd_multa ) / 100;
     return valor_final.toFixed(2);
+}
+
+export async function verifica_regra(diferenca_dias: number):Promise<IRegras[]> {
+    const regras:IRegras[] = await knex('regras_atraso')
+            .select('id_regra', 'qtd_dias_inicial', 'qtd_dias_final', 'qtd_multa', 'qtd_juros');
+    return regras.filter( regra => {
+        if(!regra.qtd_dias_final) regra.qtd_dias_final = Infinity;
+        return regra.qtd_dias_inicial < diferenca_dias && regra.qtd_dias_final >= diferenca_dias;
+    });
 }
